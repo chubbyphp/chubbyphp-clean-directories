b6007f37ceabc35634d42c01c248aefeca91f1ab
Dominik Zogg <dominik.zogg@ikmail.com>
2026-01-13 21:58:13 +0100

    phpstan level 9
    opencode (opus 4.5)

--- src/ServiceFactory/CleanDirectoriesCommandFactory.php
+++ src/ServiceFactory/CleanDirectoriesCommandFactory.php
@@ -11,8 +11,11 @@ final class CleanDirectoriesCommandFactory
 {
     public function __invoke(ContainerInterface $container): CleanDirectoriesCommand
     {
-        /** @var array<string, string> $directories */
-        $directories = $container->get('config')['directories'];
+        /** @var array{directories: array<string, string>} */
+        $config = $container->get('config');
+
+        /** @var array<string, string> */
+        $directories = $config['directories'];
 
         return new CleanDirectoriesCommand($directories);
     }
